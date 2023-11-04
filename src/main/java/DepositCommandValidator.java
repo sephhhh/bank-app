8f102d0f3e5aa24ad52a8edb7cddd3f8c95fa0c1
sephhhh <stephenwu07curry@gmail.com>
2023-11-04 05:48:10 -0400

    Added DepositingWithTwoArgumentsAsAmountWithChecking, DepositingWithTwoArgumentsAsAmountWithSavings, DepositOverOneThousandInSavings, DepositUnderZeroInSavings, DepositWithLettersInSavings

--- src/main/java/DepositCommandValidator.java
+++ src/main/java/DepositCommandValidator.java
@@ -16,6 +16,12 @@ public class DepositCommandValidator extends CommandValidator {
 		} else if ("savings".equals(account.getAccountType())) {
 			if (commandArguments.length > 3) {
 				return false;
+			} else if (!canConvertToDouble(commandArguments[2])) {
+				return false;
+			} else if (Double.parseDouble(commandArguments[2]) > 1000.0) {
+				return false;
+			} else if (Double.parseDouble(commandArguments[2]) < 0.0) {
+				return false;
 			}
 		} else if ("CD".equals(account.getAccountType())) {
 
--- src/test/java/DepositCommandValidatorTest.java
+++ src/test/java/DepositCommandValidatorTest.java
@@ -5,17 +5,22 @@ import org.junit.jupiter.api.Test;
 
 public class DepositCommandValidatorTest {
 	Bank bank;
+	Checkings checking;
+	Savings savings;
+	CertificateOfDeposit cd;
 	DepositCommandValidator depositCommandValidator;
 
 	@BeforeEach
 	void setUp() {
 		bank = new Bank();
 		depositCommandValidator = new DepositCommandValidator(bank);
+		checking = new Checkings(0.3, "12345678");
+		savings = new Savings(0.3, "12345678");
+		cd = new CertificateOfDeposit(0.3, 1000.0, "12345678");
 	}
 
 	@Test
 	void DepositingWithTwoArgumentsAsAmountWithChecking() {
-		Checkings checking = new Checkings(0.3, "12345678");
 		bank.addAccount(checking);
 		boolean actual = depositCommandValidator.validate("Deposit 12345678 500 500");
 		assertFalse(actual);
@@ -23,10 +28,29 @@ public class DepositCommandValidatorTest {
 
 	@Test
 	void DepositingWithTwoArgumentsAsAmountWithSavings() {
-		Savings savings = new Savings(0.3, "12345678");
 		bank.addAccount(savings);
 		boolean actual = depositCommandValidator.validate("Deposit 12345678 500 500");
 		assertFalse(actual);
 	}
 
+	@Test
+	void DepositOverOneThousandInSavings() {
+		bank.addAccount(savings);
+		boolean actual = depositCommandValidator.validate("Deposit 12345678 3000.0");
+		assertFalse(actual);
+	}
+
+	@Test
+	void DepositUnderZeroInSavings() {
+		bank.addAccount(savings);
+		boolean actual = depositCommandValidator.validate("Deposit 12345678 -1.0");
+		assertFalse(actual);
+	}
+
+	@Test
+	void DepositWithLettersInSavings() {
+		bank.addAccount(savings);
+		boolean actual = depositCommandValidator.validate("Deposit 12345678 abc");
+		assertFalse(actual);
+	}
 }
