55106ee8f385c230764a3aa2bd90bb076a3060ae
sephhhh <stephenwu07curry@gmail.com>
2023-11-05 03:03:39 -0500

    Made it so that the account variable in DepositCommandValidator doesn't have a strict id when trying to get an account

--- src/main/java/DepositCommandValidator.java
+++ src/main/java/DepositCommandValidator.java
@@ -8,11 +8,15 @@ public class DepositCommandValidator extends CommandValidator {
 	@Override
 	public boolean validate(String command) {
 		String[] commandArguments = command.split(" ");
-		Account account = bank.getAccountById("12345678");
+		if (commandArguments.length != 3) {
+			return false;
+		}
+		if ((commandArguments[1].length() != 8) || !canConvertToDouble(commandArguments[1])) {
+			return false;
+		}
+		Account account = bank.getAccountById(commandArguments[1]);
 		if ("checking".equals(account.getAccountType())) {
-			if (commandArguments.length != 3) {
-				return false;
-			} else if (!canConvertToDouble(commandArguments[2])) {
+			if (!canConvertToDouble(commandArguments[2])) {
 				return false;
 			} else if (Double.parseDouble(commandArguments[2]) > 2500.0) {
 				return false;
@@ -22,9 +26,7 @@ public class DepositCommandValidator extends CommandValidator {
 				return false;
 			}
 		} else if ("savings".equals(account.getAccountType())) {
-			if (commandArguments.length != 3) {
-				return false;
-			} else if (!canConvertToDouble(commandArguments[2])) {
+			if (!canConvertToDouble(commandArguments[2])) {
 				return false;
 			} else if (Double.parseDouble(commandArguments[2]) > 1000.0) {
 				return false;
