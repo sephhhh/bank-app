524f72830527e2cc07f9c494a2e55b2610a93775
sephhhh <stephenwu07curry@gmail.com>
2023-11-12 01:41:50 -0500

    Added a TDD for MasterControl

--- src/main/java/MasterControl.java
+++ src/main/java/MasterControl.java
@@ -1,2 +1,12 @@
 public class MasterControl {
+	private CommandValidator commandValidator;
+	private CommandProcessor commandProcessor;
+	private CommandStorage commandStorage;
+
+	public MasterControl(CommandValidator commandValidator, CommandProcessor commandProcessor,
+			CommandStorage commandStorage) {
+		this.commandValidator = commandValidator;
+		this.commandStorage = commandStorage;
+		this.commandProcessor = commandProcessor;
+	}
 }
--- src/test/java/MasterControlTest.java
+++ src/test/java/MasterControlTest.java
@@ -1,5 +1,12 @@
+import org.junit.jupiter.api.BeforeEach;
+
 public class MasterControlTest {
 
 	MasterControl masterControl;
 
+	@BeforeEach
+	void setUp() {
+		Bank bank = new Bank();
+		masterControl = new MasterControl(new CommandValidator(bank), new CommandProcessor(bank), new CommandStorage());
+	}
 }
